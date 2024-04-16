1cf7e0a8dd4e7a4cccb7f9d5383e0fd46d4445a9
Andrew Scala <andrew.scala@zapier.com>
2024-04-16 16:36:37 -0400

    comments

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,10 @@ import { Select, Label, Button } from 'react-aria-components';
 export default function Home() {
   return (
     <main className={styles.main}>
+      {/* I render fine in SSR.  Delete the `Select` below to see that there aren't any errors */}
       <Button css={css({padding: 30})}>Hello!</Button>
 
+      {/* Select won't render properly in SSR, possibly because of `Hidden` used in the Select component */}
       <Select>
         <Label css={css({fontWeight: 'bold'})}>Favorite Animal</Label>
       </Select>
